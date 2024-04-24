21051011ed05260bc6a196e77ec8c9080f159c0c
aruzhqnkd <aruzzhanka0264@gmail.com>
2024-04-24 14:57:47 +0500

    this is 2nd solution of lab 6

--- lab6.py
+++ lab6.py
@@ -1,33 +1,31 @@
-#  var 6. lab 6 another solution
+#   lab 6 var 6. 2nd solution
 
 # импортировка нужных библиотек
 import tkinter as tk
 from rx.subject import Subject
 
-# Создаем Subject для слайдера
-slider_subject = Subject()
-
 
 # Функция для изменения значения слайдера
-def change_slider_value(value):
+def changed_slider_value(value):
     slider_subject.on_next(value)
 
-
-# Создаем функцию-обработчик изменения слайдера
-def on_slider_change(value):
-    print(f"Текущее значение слайдера: {value}")
-
-
-# Подписываемся на изменения слайдера
-slider_subject.subscribe(on_next=on_slider_change)
 
 # Создаем графический интерфейс
 root = tk.Tk()
 root.title("Пример Слайдера")
 
 # Создаем слайдер
-slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, command=change_slider_value)
-slider.pack(pady=10)
+my_slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL)
+my_slider.pack(pady=100)
+
+# Подписываемся на изменения слайдера
+slider_subject = Subject()
+slider_subject.subscribe(lambda value: label.config(text=f"текущее значение: {value}"))
+
+my_slider.config(command=changed_slider_value)
+
+label = tk.Label(root, text="исходное значение: 0")
+label.pack()
 
 # Запускаем главный цикл событий
 root.mainloop()
